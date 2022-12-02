10fb18991bb715b38092cd8ed0cf2f273e889196
vjcitn <stvjc@channing.harvard.edu>
2022-12-02 07:07:32 -0500

    first draft

---
+++ R/testenv.R
@@ -0,0 +1,9 @@
+#' testing role of environment variables in check surveillance
+#' @param x numeric()
+#' @examples
+#' x = rnorm(20)
+#' simplefun(x)
+#' @export
+simplefun = function(x) {
+  parody::calout.detect(x)
+}
